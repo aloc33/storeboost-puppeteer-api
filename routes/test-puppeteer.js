02b04d5fdf47123ee9ab8a7e312b060e2a0e0c74
aloc33 <123343742+aloc33@users.noreply.github.com>
2025-06-05 14:01:50 -0600

    Update test-puppeteer.js

--- routes/test-puppeteer.js
+++ routes/test-puppeteer.js
@@ -1,25 +1,30 @@
+import chromium from 'chrome-aws-lambda';
 import express from 'express';
-import puppeteer from 'puppeteer-core';
-import { executablePath } from 'puppeteer';
 
 const router = express.Router();
 
 router.get('/api/test-puppeteer', async (req, res) => {
+  let browser = null;
+
   try {
-    const browser = await puppeteer.launch({
-      headless: 'new',
-      executablePath: executablePath(), // Use Puppeteer's built-in Chromium
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
     });
 
     const page = await browser.newPage();
     await page.goto('https://example.com');
     const title = await page.title();
-    await browser.close();
 
     res.json({ success: true, title });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
 });
 

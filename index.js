b22589c70b2265594e996f8e8a610a05cfcb8b8a
aloc33 <123343742+aloc33@users.noreply.github.com>
2025-06-06 15:20:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 10000;
 
 app.get('/api/test-puppeteer', handler);
 
+app.get('/', (_, res) => {
+  res.send('Storeboost Puppeteer API is live');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

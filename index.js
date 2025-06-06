58c7678081ba5b7dd2ce79c260fae7298f8cc8d6
aloc33 <123343742+aloc33@users.noreply.github.com>
2025-06-06 15:40:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { handler } from './api/test-puppeteer.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get('/api/test-puppeteer', handler);
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Storeboost Puppeteer API is live');
 });
 

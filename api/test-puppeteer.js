790bfff4ab7439f57d7871a18ebc8e48d1670355
aloc33 <123343742+aloc33@users.noreply.github.com>
2025-06-06 12:19:19 -0600

    Update test-puppeteer.js

--- api/test-puppeteer.js
+++ api/test-puppeteer.js
@@ -1,25 +1,22 @@
 import chromium from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
 
-export const handler = async (req, res) => {
+export async function handler(req, res) {
   try {
-    const executablePath = await chromium.executablePath;
-
     const browser = await puppeteer.launch({
       args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath,
+      executablePath: await chromium.executablePath,
       headless: chromium.headless,
+      defaultViewport: chromium.defaultViewport,
     });
 
     const page = await browser.newPage();
     await page.goto('https://example.com');
     const title = await page.title();
-
     await browser.close();
 
     res.json({ success: true, title });
   } catch (error) {
     res.json({ success: false, error: error.message });
   }
-};
+}

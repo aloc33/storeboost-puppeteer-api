894d51d126b3ea69845122723920861d61306f50
aloc33 <123343742+aloc33@users.noreply.github.com>
2025-06-12 10:29:06 -0600

    Update test-puppeteer.js

--- api/test-puppeteer.js
+++ api/test-puppeteer.js
@@ -6,14 +6,14 @@ const router = express.Router();
 router.get('/test-puppeteer', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: puppeteer.executablePath() // <- USE BUNDLED CHROMIUM
+      headless: "new", // or true
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
 
     const page = await browser.newPage();
     await page.goto('https://example.com');
     const title = await page.title();
+
     await browser.close();
 
     res.json({ success: true, title });
